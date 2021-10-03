bf97faf0e884bd170203484d910b9c9f1cf080ab
Kauaferr <88683510+Kauaferr@users.noreply.github.com>
2021-10-03 12:09:09 -0300

    Update JPAUtil.java

--- src/main/java/fazendo2/JPAUtil.java
+++ src/main/java/fazendo2/JPAUtil.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class JPAUtil {
-	private static final EntityManagerFactory entidade = Persistence.createEntityManagerFactory("fazendo2");
+	private static final EntityManagerFactory entidade = Persistence.createEntityManagerFactory("cadastro");
 
 	
 	public JPAUtil() throws Exception {
